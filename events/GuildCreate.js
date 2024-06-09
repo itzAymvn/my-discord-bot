32285936d3c894962a6bb65340bb6afd8873b582
Aymvn <aymanbdouzi@gmail.com>
2024-06-09 17:48:17 +0100

    feat: Add GuildCreate event and update status when bot joins a guild

---
+++ events/GuildCreate.js
@@ -0,0 +1,22 @@
+const { Events, ActivityType, Client } = require("discord.js")
+
+module.exports = {
+	name: Events.GuildCreate,
+
+	/**
+	 * Executes the GuildCreate event.
+	 * Sets the activity of the client user to display the total number of servers.
+	 *
+	 * @param {Client} client - The Discord client.
+	 */
+	execute(client) {
+		const totalServers = client.guilds.cache.size
+		const totalServersText = `over ${totalServers} server${
+			totalServers === 1 ? "" : "s"
+		}`
+
+		client.user.setActivity(totalServersText, {
+			type: ActivityType.Watching,
+		})
+	},
+}
